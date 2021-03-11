384a8737fb741ce8d4c0844700f3eb4e5769f359
Naaji Hylton <naajihylton@dynamic-oit-vapornet100-10-9-68-204.princeton.edu>
2021-03-10 19:25:34 -0500

    added small/large distraction train test

--- networkTests/testUtils/plot.py
+++ networkTests/testUtils/plot.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 
 # Compares and plots the loss of four different networks (for testing)
-def plot_four_losses(title, loss1, loss2, loss3, loss4):
+def plot_four_losses(title, loss1, loss2=None, loss3=None, loss4=None):
     # Plot mean losses
     # plt.figure()
     plt.suptitle(title)
     plt.plot(loss1, color='red')
-    plt.plot(loss2, color='blue')
-    plt.plot(loss3, color='orange')
-    plt.plot(loss4, color='green')
+    if loss2 is not None:
+        plt.plot(loss2, color='blue')
+    if loss3 is not None:
+        plt.plot(loss3, color='orange')
+    if loss4 is not None:
+        plt.plot(loss4, color='green')
     plt.xlabel('epoch')
     plt.ylabel('mean loss')
