e32be4452687e3a61ba39850952ae3de68f10519
Naaji Hylton <naajihylton@dynamic-oit-vapornet100-10-9-68-204.princeton.edu>
2021-03-18 20:11:51 -0400

    updated Test 7

--- networkTests/testUtils/plot.py
+++ networkTests/testUtils/plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-# Compares and plots the loss of four different networks (for testing)
+# Compares and plots the loss of four different networks
 def plot_four_losses(title, loss1, loss2=None, loss3=None, loss4=None):
     # Plot mean losses
     # plt.figure()
@@ -14,3 +14,23 @@ def plot_four_losses(title, loss1, loss2=None, loss3=None, loss4=None):
         plt.plot(loss4, color='green')
     plt.xlabel('epoch')
     plt.ylabel('mean loss')
+    
+# Plots the mean loss of up to four different networks
+def plot_mean_losses(title, loss1, loss2=None, loss3=None, loss4=None, *args, **kwargs):
+    # Plot mean losses
+    # plt.figure()
+    plt.suptitle(title)
+    plt.plot(loss1, color='red', *args, **kwargs)
+    if loss2 is not None:
+        plt.plot(loss2, color='blue', *args, **kwargs)
+    if loss3 is not None:
+        plt.plot(loss3, color='orange', *args, **kwargs)
+    if loss4 is not None:
+        plt.plot(loss4, color='green', *args, **kwargs)
+    plt.xlabel('epoch')
+    plt.ylabel('mean loss')
+    
+# Plots the individual loss of all the networks of a specific type
+def plot_individual_losses(losses, *args, **kwargs):
+    for loss in losses:
+        plt.plot(loss, *args, **kwargs)
